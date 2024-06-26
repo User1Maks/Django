e53f90f3c8a7ac8b31496f3c5420ae1c6497b7c4
Maksim Kamenev <maksim-kamenev96@mail.ru>
2024-06-26 13:52:05 +0300

    CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import home, contacts, products_detail
+from catalog.views import ProductsListView, ContactsView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("products/<int:pk>/", products_detail, name="products_detail"),
+    path("", ProductsListView.as_view(), name="base"),
+    path("contacts/", ContactsView.as_view(), name="contacts"),
+    path("products/<int:pk>/", ProductDetailView.as_view(), name="products_detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,29 +1,43 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import render
 from catalog.models import Product
+from django.views.generic import ListView, DetailView, View
 
 
-def home(requests):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(requests, "products_list.html", context)
+class ProductsListView(ListView):
+    model = Product
+
+    # app_name/<model_name>_<action>
+    # catalog/product_list.html
 
 
-def contacts(requests):
-    if requests.method == "POST":
-        name = requests.POST.get("name")
-        phone = requests.POST.get("phone")
-        message = requests.POST.get("message")
-        print(f"{name}: {phone}\n {message}")
-
-    return render(requests, "contacts.html")
+class ProductDetailView(DetailView):
+    model = Product
 
 
-def products_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {
-        "product": product
-    }
-    return render(request, 'products_detail.html', context)
+class ContactsView(View):
+    def get(self, requests):
+        return render(requests, "catalog/contacts.html")
+
+# def products_list(requests):
+#     product_list = Product.objects.all()
+#     context = {
+#         'object_list': product_list
+#     }
+#     return render(requests, "products_list.html", context)
+
+
+# def products_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {
+#         "product": product
+#     }
+#     return render(request, 'product_detail.html', context)
+
+# def contacts(requests):
+#     if requests.method == "POST":
+#         name = requests.POST.get("name")
+#         phone = requests.POST.get("phone")
+#         message = requests.POST.get("message")
+#         print(f"{name}: {phone}\n {message}")
+#
+#     return render(requests, "catalog/contacts.html")
